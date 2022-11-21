fed664606c2a02df42172fa3d0831a41f863a41b
Arastryx <arastryx.dravoga@gmail.com>
2022-11-20 21:31:54 -0400

    -Added fonts and borders and stuff idk my brain is empty right now lol

--- src/ArastryxSite/Common/Gallery/Entry.tsx
+++ src/ArastryxSite/Common/Gallery/Entry.tsx
@@ -3,9 +3,33 @@ import { GalleryEntry } from "./interfaces";
 import { Box, Stack, styled } from "@mui/system";
 import { Card } from "./Card";
 
-const GalleryImage = styled("img")({
-  maxWidth: "calc(100vw - 500px)",
-  maxHeight: "calc(100vh - 150px)",
+interface GalleryImageProps {
+  hasHeader?: boolean;
+  hasDesc?: boolean;
+}
+
+const GalleryImage = styled("img")<GalleryImageProps>(
+  ({ hasHeader, hasDesc }) => ({
+    maxWidth: `calc(100vw - ${hasDesc ? 500 : 50}px)`,
+    maxHeight: `calc(100vh - ${hasHeader ? 220 : 100}px)`,
+    minHeight: 500,
+  })
+);
+
+const ImageHeader = styled("h1")({
+  textAlign: "center",
+  position: "relative",
+  "&::before": {
+    content: '""',
+    position: "absolute",
+    top: "100%",
+    width: "100%",
+    left: 0,
+    height: 1,
+    borderRadius: 2,
+    background:
+      "linear-gradient(90deg, #fffae900 10%, #fffae9 50%, #fffae900 90%)",
+  },
 });
 
 export interface EntryProps {
@@ -15,11 +39,18 @@ export interface EntryProps {
 export const Entry: React.FC<EntryProps> = ({ entry }) => {
   return (
     <Stack direction={{ lg: "row" }} gap={4}>
-      <Stack flex={1} alignItems="center">
-        <Card flex={1} p={5}>
+      <Stack flex={1} alignItems="center" gap={2}>
+        {entry.name && (
+          <Card alignSelf="stretch">
+            <ImageHeader>{entry.name}</ImageHeader>
+          </Card>
+        )}
+        <Card flex={1} p={2}>
           <GalleryImage
             src={encodeURI(`/dist/gallery/${entry.image}`)}
             alt={entry.image}
+            hasHeader={entry.name != null}
+            hasDesc={entry.description != null}
           />
         </Card>
       </Stack>
--- src/ArastryxSite/GalleryPage/galleryEntries.tsx
+++ src/ArastryxSite/GalleryPage/galleryEntries.tsx
@@ -1,6 +1,18 @@
+import { Box } from "@mui/system";
 import { GalleryEntry } from "../Common/Gallery/interfaces";
 
 export const galleryEntries: GalleryEntry[] = [
+  {
+    image: "AWolfInAutumn.jpg",
+    slug: "a_wolf_in_autumn",
+    name: "A Wolf in Autumn",
+    description: (
+      <Box>
+        <p>This is</p>
+        <p>A test</p>
+      </Box>
+    ),
+  },
   {
     image: "SUNDERED INFERNO.jpg",
     slug: "sundered_inferno",
